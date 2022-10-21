d3e52562425dc170094894ba2e950932b7892000
Vanitha-rk <114817847+Vanitha-rk@users.noreply.github.com>
2022-10-21 12:41:13 +0530

    Add files via upload

---
+++ week3day2/org/college/College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("ABC College of Engineering");
+	}
+	
+	public void collegeCode() {
+		System.out.println("9870");
+	}
+	public void collegeRank() {
+		System.out.println("10th rank");
+	}
+	
+	
+	
+	
+}
---
+++ week3day2/org/system/Computer.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class Computer 
+{
+	
+	
+	public void computerModel() {
+		System.out.println("Lenovo Ideacentre 520 Core i3 (6th Gen)");
+	
+	}
+
+	
+
+}
